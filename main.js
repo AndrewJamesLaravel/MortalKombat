7196a127ca995e8e5e2b6765e380c49a32e67179
andrew <chibarandrey@gmail.com>
2021-12-09 01:46:31 +0300

    homework-2

---
+++ main.js
@@ -0,0 +1,70 @@
+const player1 = {
+  name: 'Skorpion',
+  hp: 80,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+  weapon: ['axe', 'katana', 'knife'],
+  attack: function () {
+    console.log(`<h3>${name}</h3> <h2>Fight...</h2>`);
+  }
+}
+
+const player2 = {
+  name: 'Liukang',
+  hp: 60,
+  img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+  weapon: ['axe', 'katana', 'knife'],
+  attack: function () {
+    console.log(`<h3>${name}</h3> <h2>Fight...</h2>`);
+  }
+}
+
+function createPlayer (playerClass, player) {
+  const $player = document.createElement('div');
+  $player.classList.add(playerClass);
+
+  const $progressBar = document.createElement('div');
+  $progressBar.classList.add('progressbar');
+
+  const $life = document.createElement('div');
+  $life.classList.add('life');
+  $life.style.width = '100%';
+
+  const $extraLife = document.createElement('div');
+  $extraLife.classList.add('extralife');
+  $extraLife.style.width = '100%';
+  $extraLife.style.height = '2em';
+
+  const $div = document.createElement('div');
+  $div.style.color = '#fff';
+  $div.style.fontSize = '20px';
+  $div.style.padding = '5px';
+  $div.style.fontWeight = '600';
+  $div.innerText = player.hp;
+  $extraLife.appendChild($div);
+
+const $name = document.createElement('div');
+  $name.classList.add('name');
+  $name.innerText = player.name;
+
+  $progressBar.appendChild($life);
+  $progressBar.appendChild($extraLife);
+  $progressBar.appendChild($name);
+
+const $character = document.createElement('div');
+  $character.classList.add('character');
+
+  const $playerImg = document.createElement('img');
+  $playerImg.src = player.img;
+  $character.appendChild($playerImg);
+
+  $player.appendChild($progressBar);
+  $player.appendChild($character);
+
+  const $arena = document.querySelector('.arenas');
+
+  $arena.appendChild($player);
+
+}
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
